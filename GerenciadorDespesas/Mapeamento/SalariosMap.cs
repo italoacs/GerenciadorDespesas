fc24a7df985f813dc244826eb247def282a124f2
Italo Soares <italo.soares@serel.com.br>
2019-07-25 13:41:31 -0300

    Adicionar arquivos de projeto.

---
+++ GerenciadorDespesas/Mapeamento/SalariosMap.cs
@@ -0,0 +1,18 @@
+﻿using GerenciadorDespesas.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorDespesas.Mapeamento
+{
+    public class SalariosMap : IEntityTypeConfiguration<TipoDespesas>
+    {
+        public void Configure(EntityTypeBuilder<TipoDespesas> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
